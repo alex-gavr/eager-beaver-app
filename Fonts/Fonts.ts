a58388aa5cd11a52f227d38b8d38cd282ae37907
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-23 16:45:16 +0200

    init project

---
+++ Fonts/Fonts.ts
@@ -0,0 +1,12 @@
+import localFont from 'next/font/local';
+
+export const KoskoBold = localFont({
+    src: '../fonts/KoskoBold.ttf',
+    variable: '--ff-heading',
+    display: 'swap',
+});
+export const KoskoRegular = localFont({
+    src: '../fonts/KoskoRegular.ttf',
+    variable: '--ff-body',
+    display: 'swap',
+});
