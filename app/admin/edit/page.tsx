0a639d0d70eab14b24fc32d87d63e52139cc659d
Alexander Gavrilenko <sashadrug@mail.ru>
2023-05-29 18:45:14 +0200

    protect admin

---
+++ app/admin/edit/page.tsx
@@ -0,0 +1,12 @@
+
+interface IPageProps {}
+
+const Page = ({}: IPageProps) => {
+  return (
+    <div>
+      <h1>page to edit</h1>
+    </div>
+  );
+};
+
+export default Page;
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,17 +1,51 @@
-import { ReactNode } from 'react';
-import { ToastContainer } from 'react-toastify';
+'use client';
+import { ReactNode, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import '@uploadthing/react/styles.css';
+import Button from '@/components/buttons/button';
 
 interface ILayoutProps {
   children: ReactNode;
 }
 
+const NotAllowed = () => {
+  return (
+    <div className='flex min-h-[90vh] w-full flex-col items-center justify-center'>
+      <h1 className='text-center text-2xl text-red-500 lg:text-4xl'>you are not allowed</h1>
+    </div>
+  );
+};
+
 const Layout = ({ children }: ILayoutProps) => {
+  const [auth, setAuth] = useState<boolean | null>(null);
+  const [password, setPassword] = useState<string | null>();
+
+  const handleLogin = () => {
+    if (password === '265d46cc1c7501afff17aa87030cba8639cb9dbf2fa7d99c4301801da881d42a') {
+      setAuth(true);
+    }
+  };
+
   return (
     <>
-      <ToastContainer />
-      <>{children}</>
+      {auth === true ? (
+        <>{children}</>
+      ) : auth === false ? (
+        <NotAllowed />
+      ) : (
+        <div className='flex min-h-[90vh] w-full flex-col items-center justify-center'>
+          <h1 className='text-center text-2xl text-red-500 lg:text-4xl'>Do you know a password</h1>
+          <div className='flex flex-col items-center justify-center'>
+            <input
+              className='mb-4'
+              type='password'
+              placeholder='password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={handleLogin}>login</Button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
